8f4c8e07d48a42b0b8d7f5530946e0b8089695d7
almarfeta <almareran@gmail.com>
2025-01-25 13:55:39 +0200

    Add NavBarComponent

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-  <router-view />
+  <div id="app">
+    <NavBarComponent />
+    <router-view />
+  </div>
 </template>
+
+<script>
+import NavBarComponent from "@/components/NavBarComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavBarComponent,
+  },
+};
+</script>
+
+<style>
+/* Insert global styling here... */
+</style>
---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">LibraryApp</a>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link" exact>Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/books" class="nav-link">Books</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBarComponent",
+};
+</script>
