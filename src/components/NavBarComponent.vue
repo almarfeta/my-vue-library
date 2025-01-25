8cae6ac5185bfc89232535a03ef6fcc77138e2b4
almarfeta <almareran@gmail.com>
2025-01-25 14:14:39 +0200

    Rename app

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">LibraryApp</a>
+      <a class="navbar-brand" href="#">MyVueLibrary</a>
 
       <button
         class="navbar-toggler"
