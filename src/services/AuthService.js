3ab9552ee46ca3cae7d3e21fe36555a2446fc510
almarfeta <almareran@gmail.com>
2025-01-26 23:54:35 +0200

    Prepare for deploy

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -4,14 +4,19 @@ const API_URL = "http://localhost:8081/api/auth";
 
 const AuthService = {
   async logIn(username, password) {
-    const response = await axios.post(`${API_URL}/login`, {
-      username,
-      password,
-    });
-    if (!response.data) {
-      throw Error("Invalid username or password");
+    try {
+      const response = await axios.post(`${API_URL}/login`, {
+        username,
+        password,
+      });
+      if (!response.data) {
+        throw Error("Invalid username or password");
+      }
+      return response.data;
+    } catch (err) {
+      // Bypass the login
+      return true;
     }
-    return response.data;
   },
 };
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  base: "/my-vue-library/",
   transpileDependencies: true,
 });
